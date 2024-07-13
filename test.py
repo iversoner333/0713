67ee4f1f623bbdc6fddb010c513e5968d9de0282
Juice <iversoner333@gmail.com>
2024-07-13 12:03:48 +0800

    install new fuction

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import sys #查一下sys是幹嘛的??
+import sys #查一下sys是幹嘛的?? 用於回報程式有錯誤時可以使用
+import mediapipe
 print("hello world")
 a=1
 b=2
@@ -8,6 +9,6 @@ a=[1,2,4,3]
 for i in a:
     print(i)
 
-sys.exit(135)
+sys.exit(0)
 
     
